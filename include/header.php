278f7511724a65d54bab41efcdf12babda2ffe33
vitordbo <vitorduarte177@gmail.com>
2022-12-14 14:28:42 -0300

    first

---
+++ include/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+        <script src="https://code.jquery.com/jquery-3.6.2.slim.js" integrity="sha256-OflJKW8Z8amEUuCaflBZJ4GOg4+JnNh9JdVfoV+6biw=" crossorigin="anonymous"></script>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css" integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-info bg-info">
+            <h5 class="text-white">Hospital Management System</h5>
+
+            <div class="mr-auto"></div>
+
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li>
+                <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
+                <li class="nav-item"><a href="#" class="nav-link text-white">Patient</a></li>            
+            </ul>
+        </nav>
+    
+    </body>
+</html>
